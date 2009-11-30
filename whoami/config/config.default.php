364259c4920456214ac63a30dd0cd91b2db0f1e1
nevali <mo@nevali.net>
2009-11-30 08:25:05 +0000

    Define SCRIPTS_USE_GAPI to use googleapis.com to serve jQuery; add missing Chroma-Hash submodule.

--- whoami/config/config.default.php
+++ whoami/config/config.default.php
@@ -10,6 +10,13 @@
  * all copies of a particular application.
  */
 
+/* Use googleapis.com for jQuery: if you do not define this, the platform will
+ * expect to be able to find jQuery in /jquery/x.y.z/jquery.min.js (where x.y.z
+ * is the version number).
+ */
+ 
+define('SCRIPTS_USE_GAPI', true);
+
 $BUILTIN_USERS = array(
 	'test' => array(
 		'uuid' => 'd0f855cd-7c2d-42a7-81f1-b0505d77b70e',
